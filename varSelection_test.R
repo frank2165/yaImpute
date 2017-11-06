323926af0f4f1855ed7b30e3b1931d46d239d616
frank2165 <marc.fletcher1990@gmail.com>
2017-11-07 09:47:12 +1000

    Testing changes made to yaImpute functions
    Final version of test:
    Both the modified and package version are run on a test dataset. The run-time of each method is printed and the hashed output is compared to ensure the results are the same.
    
    Note: for a reliable comparison of output the bootstrap option was set to FALSE.

--- varSelection_test.R
+++ varSelection_test.R
@@ -32,27 +32,33 @@ for(i in seq_along(env.list)){
 # Using the future package to simulate threads in R, this way the first test will not block the
 # commencement of the second. (have to be careful that the number of cores being used does not
 # exceed the number available).
-plan(multiprocess)
+plan(multisession)
 
 
 # Test package version, on Windows this will run with useParallel == FALSE.
 pkg.result %<-% {
+    set.seed(117)
     pkg.start <- Sys.time()
-    pkg.tmp   <- yaImpute::varSelection(x = x, y = y, yaiMethod = yaiMethod, trace = trace,
-                                        k = k, nboot = nboot, pVal = 0.05, rfMode = "regression",
-                                        ntree = 500, useParallel = FALSE)
-    pkg.runtime <- Sys.time() - start
+    pkg.tmp   <- suppressMessages(
+        yaImpute::varSelection(x = x, y = y, yaiMethod = yaiMethod, trace = trace,
+                               k = k, nboot = nboot, pVal = 0.05, rfMode = "regression",
+                               ntree = 500, useParallel = FALSE, bootstrap = FALSE)
+    )
+    pkg.runtime <- Sys.time() - pkg.start
     list(start = pkg.start, result = pkg.tmp, runtime = pkg.runtime)
 }
 
 
 # Test modified version
 new.result %<-% {
+    set.seed(117)
     new.start <- Sys.time()
-    new.tmp   <- varSelection(x = x, y = y, yaiMethod = yaiMethod, trace = trace,
-                              k = k, nboot = nboot, pVal = 0.05, rfMode = "regression",
-                              ntree = 500)
-    new.runtime <- Sys.time() - start
+    new.tmp   <- suppressMessages(
+        varSelection(x = x, y = y, yaiMethod = yaiMethod, trace = trace,
+                     k = k, nboot = nboot, pVal = 0.05, rfMode = "regression",
+                     ntree = 500, bootstrap = FALSE)
+    )
+    new.runtime <- Sys.time() - new.start
     list(start = new.start, result = new.tmp, runtime = new.runtime)
 }
 
